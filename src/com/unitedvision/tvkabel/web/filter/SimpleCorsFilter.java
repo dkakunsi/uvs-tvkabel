112b6c3e6f7b133d53ce5096f030d77aa653f246
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2014-11-26 23:31:29 +0800

    Using Tomcat's CORS implementation

--- src/com/unitedvision/tvkabel/web/filter/SimpleCorsFilter.java
+++ src/com/unitedvision/tvkabel/web/filter/SimpleCorsFilter.java
@@ -2,7 +2,6 @@ package com.unitedvision.tvkabel.web.filter;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
@@ -10,12 +9,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
-public class SimpleCorsFilter implements Filter {
+public class SimpleCorsFilter {
 
-	@Override
 	public void destroy() { }
 
-	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
@@ -25,7 +22,6 @@ public class SimpleCorsFilter implements Filter {
 		chain.doFilter(req, res);
 	}
 
-	@Override
 	public void init(FilterConfig arg0) throws ServletException { }
 
 }
