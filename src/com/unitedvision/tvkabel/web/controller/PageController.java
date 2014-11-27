97a3ea13f985f7b7d01169bc7e1899e7a105c376
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2014-11-27 12:16:28 +0800

    Create /api/login and /api/logout. Used by Rest client to authenticate.

--- src/com/unitedvision/tvkabel/web/controller/PageController.java
+++ src/com/unitedvision/tvkabel/web/controller/PageController.java
@@ -21,8 +21,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.unitedvision.tvkabel.core.domain.Pegawai;
 import com.unitedvision.tvkabel.exception.ApplicationException;
+import com.unitedvision.tvkabel.exception.EntityNotExistException;
+import com.unitedvision.tvkabel.exception.UnauthenticatedAccessException;
 import com.unitedvision.tvkabel.util.CodeUtil;
+import com.unitedvision.tvkabel.web.model.PegawaiModel;
+import com.unitedvision.tvkabel.web.rest.PegawaiRestResult;
 import com.unitedvision.tvkabel.web.rest.RestResult;
 
 @Controller
@@ -42,20 +47,45 @@ public class PageController extends AbstractController {
 		return "cover_login";
 	}
 	
+	@RequestMapping(value = "/api/login", method = RequestMethod.POST)
+	public @ResponseBody PegawaiRestResult loginApi(@RequestParam String username, @RequestParam String password) throws UnauthenticatedAccessException {
+		try {
+			Pegawai pegawai = pegawaiService.getOneByUsername(username);
+			if (pegawai.authenticate(password))
+				return PegawaiRestResult.create("Berhasil!", pegawai.toModel());
+			throw new UnauthenticatedAccessException("Operator could not be authenticated");
+		} catch (EntityNotExistException e) {
+			return PegawaiRestResult.create(String.format("Gagal! %s", e.getMessage()));
+		}
+	}
+	
+	@RequestMapping(value = "/api/logout", method = RequestMethod.POST)
+	public @ResponseBody PegawaiRestResult logoutApi(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			logoutProcess(request, response);
+			
+			return PegawaiRestResult.create("Berhasil", PegawaiModel.createGuest());
+		} catch (IOException e) {
+			return PegawaiRestResult.create(String.format("Gagal! %s", e.getMessage()));
+		}
+	}
+	
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			logoutProcess(request, response);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "redirect:/";
+	}
+	
+	private void logoutProcess(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		
 		if (auth != null)
 			new SecurityContextLogoutHandler().logout(request, response, auth);
-
-		try {
-			clearBasicAuthentication();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return "redirect:/";
+		clearBasicAuthentication();
 	}
 	
 	private void clearBasicAuthentication() throws IOException {
