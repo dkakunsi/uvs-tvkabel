02594476a690dd4e3fbc1f64f71ca40394fd9ed7
Deddy Kakunsi <deddykakunsi@outlook.com>
2015-04-30 16:22:15 +0800

    tambah alat pada database.

--- unitedvision_tvkabel.sql
+++ unitedvision_tvkabel.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS `kecamatan` (
   `id_kota` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_kota` (`id_kota`,`nama`)
+  FOREIGN KEY `kota` (`id_kota`) REFERENCES `kota`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
 
 DROP TABLE IF EXISTS `kelurahan`;
@@ -17,6 +18,7 @@ CREATE TABLE IF NOT EXISTS `kelurahan` (
   `id_kecamatan` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_kecamatan` (`id_kecamatan`,`nama`)
+  FOREIGN KEY `kecamatan` (`id_kecamatan`) REFERENCES `kecamatan`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=118 ;
 
 DROP TABLE IF EXISTS `kota`;
@@ -39,8 +41,10 @@ CREATE TABLE IF NOT EXISTS `pegawai` (
   `role` int(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `kode` (`id_perusahaan`,`kode`),
+  UNIQUE KEY `nama` (`id_perusahaan`,`nama`),
   UNIQUE KEY `username` (`username`),
   KEY `id_perusahaan` (`id_perusahaan`)
+  FOREIGN KEY `perusahaan` (`id_perusahaan`) REFERENCES `perusahaan`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 DROP TABLE IF EXISTS `pelanggan`;
@@ -65,6 +69,7 @@ CREATE TABLE IF NOT EXISTS `pelanggan` (
   `iuran` mediumtext NOT NULL,
   `pembayaran_terakhir` int(11) DEFAULT NULL,
   `tunggakan` int(2) NOT NULL,
+  `id_source` int(11),
   PRIMARY KEY (`id`),
   UNIQUE KEY `kode` (`kode`,`id_perusahaan`),
   UNIQUE KEY `nama` (`id_perusahaan`,`nama`),
@@ -72,9 +77,10 @@ CREATE TABLE IF NOT EXISTS `pelanggan` (
   KEY `id_kelurahan` (`id_kelurahan`),
   KEY `id_perusahaan` (`id_perusahaan`),
   KEY `pembayaran_terakhir` (`pembayaran_terakhir`)
+  FOREIGN KEY `perusahaan` (`id_perusahaan`) REFERENCES `perusahaan`(`id`) ON DELETE SET NULL
+  FOREIGN KEY `kelurahan` (`id_kelurahan`) REFERENCES `kelurahan`(`id`) ON DELETE SET NULL
+  FOREIGN KEY `source` (`id_source`) REFERENCES `alat`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=922 ;
-
-
 
 DROP TABLE IF EXISTS `pembayaran`;
 CREATE TABLE IF NOT EXISTS `pembayaran` (
@@ -89,7 +95,8 @@ CREATE TABLE IF NOT EXISTS `pembayaran` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `kode_referensi` (`kode_referensi`),
   UNIQUE KEY `id_pelanggan` (`id_pelanggan`,`tahun`,`bulan`),
-  KEY `id_pegawai` (`id_pegawai`)
+  FOREIGN KEY `pelanggan` (`id_pelanggan`) REFERENCES `pelanggan`(`id`) ON DELETE SET NULL
+  FOREIGN KEY `pegawai` (`id_pegawai`) REFERENCES `pegawai`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12471 ;
 
 DROP TABLE IF EXISTS `perusahaan`;
@@ -111,82 +118,49 @@ CREATE TABLE IF NOT EXISTS `perusahaan` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `kode` (`kode`),
   UNIQUE KEY `email` (`email`),
-  KEY `id_kelurahan` (`id_kelurahan`)
+  FOREIGN KEY `kelurahan` (`id_kelurahan`) REFERENCES `kelurahan`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 DROP TABLE IF EXISTS `history`;
 CREATE TABLE IF NOT EXISTS `history` (
-`id	` int(11) NOT NULL AUTO_INCREMENT,
-`id_pelanggan` int(11) NOT NULL,
-`tanggal` DATE NOT NULL,
-`status` int(1) NOT NULL,
-`keterangan` varchar(255),
-`jumlah_aktif` int(11) NOT NULL,
-`jumlah_putus` int(11) NOT NULL,
-`jumlah_berhenti` int(11) NOT NULL,
-`jumlah_gratis` int(11) NOT NULL,
-PRIMARY KEY(`id`),
-KEY (`id_pelanggan`)
+  `id	` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pelanggan` int(11) NOT NULL,
+  `tanggal` DATE NOT NULL,
+  `status` int(1) NOT NULL,
+  `keterangan` varchar(255),
+  `jumlah_aktif` int(11) NOT NULL,
+  `jumlah_putus` int(11) NOT NULL,
+  `jumlah_berhenti` int(11) NOT NULL,
+  `jumlah_gratis` int(11) NOT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY `pelanggan` (`id_pelanggan`) REFERENCES `pelanggan`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 DROP TABLE IF EXISTS `token`;
 CREATE TABLE IF NOT EXISTS `token` (
-`token` VARCHAR(255) NOT NULL,
-`id_pegawai` INT(11) NOT NULL,
-`tanggal` DATE NOT NULL,
-`expire` DATE NOT NULL,
-`status` INT(1) NOT NULL,
-PRIMARY KEY(`token`),
-KEY (`id_pegawai`)
+  `token` VARCHAR(255) NOT NULL,
+  `id_pegawai` INT(11) NOT NULL,
+  `tanggal` DATE NOT NULL,
+  `expire` DATE NOT NULL,
+  `status` INT(1) NOT NULL,
+  PRIMARY KEY(`token`),
+  FOREIGN KEY `pegawai` (`id_pegawai`) REFERENCES `pegawai`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
---
--- Constraints for table `kecamatan`
---
-ALTER TABLE `kecamatan`
-  ADD CONSTRAINT `kecamatan_ibfk_1` FOREIGN KEY (`id_kota`) REFERENCES `kota` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `kelurahan`
---
-ALTER TABLE `kelurahan`
-  ADD CONSTRAINT `kelurahan_ibfk_1` FOREIGN KEY (`id_kecamatan`) REFERENCES `kecamatan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `pegawai`
---
-ALTER TABLE `pegawai`
-  ADD CONSTRAINT `pegawai_ibfk_1` FOREIGN KEY (`id_perusahaan`) REFERENCES `perusahaan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `pelanggan`
---
-ALTER TABLE `pelanggan`
-  ADD CONSTRAINT `pelanggan_ibfk_1` FOREIGN KEY (`id_kelurahan`) REFERENCES `kelurahan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pelanggan_perusahaan_ibfk_2` FOREIGN KEY (`id_perusahaan`) REFERENCES `perusahaan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pembayaran_terakhir` FOREIGN KEY (`pembayaran_terakhir`) REFERENCES `pembayaran` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `pembayaran`
---
-ALTER TABLE `pembayaran`
-  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pembayaran_ibfk_2` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `perusahaan`
---
-ALTER TABLE `perusahaan`
-  ADD CONSTRAINT `perusahaan_ibfk_1` FOREIGN KEY (`id_kelurahan`) REFERENCES `kelurahan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `history`
---
-ALTER TABLE `history`
-  ADD CONSTRAINT `pelanggan_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `history`
---
-ALTER TABLE `token`
-  ADD CONSTRAINT `pegawai_ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+DROP TABLE IF EXISTS `alat`;
+CREATE TABLE IF NOT EXISTS `alat` (
+  `id` int(11) NOT NULL,
+  `id_perusahaan` int(11) NOT NULL,
+  `kode` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tipe` int(1) NOT NULL,
+  `deskripsi` varchar(255),
+  `latitude` int(11) DEFAULT 0,
+  `longitude` int(11) DEFAULT 0,
+  `id_source` int(11),
+  PRIMARY KEY(id),
+  UNIQUE KEY `perusahaan_kode` (`id_perusahaan`,`kode`),
+  UNIQUE KEY `perusahaan_nama` (`id_perusahaan`,`nama`),
+  FOREIGN KEY `perusahaan` (`id_perusahaan`) REFERENCES `perusahaan`(`id`) ON DELETE SET NULL
+  FOREIGN KEY `source` (`id_source`) REFERENCES `alat`(`id`) ON DELETE SET NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
